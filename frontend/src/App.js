872aceff44e0bb6d96f20b1b53e3dc384325a6c3
iamkishan08 <kishanoffcl08@gmail.com>
2024-10-22 15:18:43 +0530

    commit m

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Header from './components/header';
+import Sidebar from './components/sidebar';
+import Master from './components/master';
+import ScheduleTask from './components/scheduleTask';
+import ViewTask from './components/viewTask';
 import './App.css';
 
 function App() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className={`app ${isSidebarOpen ? 'sidebar-open' : ''}`}>
+      <Header toggleSidebar={toggleSidebar} />
+      <Sidebar isSidebarOpen={isSidebarOpen} />
+      <div className="content">
+        <Routes>
+          <Route path="/master" element={<Master />} />
+          <Route path="/schedule-task" element={<ScheduleTask />} />
+          <Route path="/view-task" element={<ViewTask />} />
+          <Route path="/" element={<h3>Welcome to the Patch Management Report Tool</h3>} />
+        </Routes>
+      </div>
     </div>
+  </Router>
   );
 }
 
